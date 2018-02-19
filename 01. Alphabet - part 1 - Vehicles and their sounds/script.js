9270d7c7ffd30eebeedb720f94528680ce1ec19d
Filip Stangricki <stangricki@gmail.com>
2018-02-19 12:40:37 +0100

    Opacity of images on audio end

--- 01. Alphabet - part 1 - Vehicles and their sounds/script.js
+++ 01. Alphabet - part 1 - Vehicles and their sounds/script.js
@@ -1,13 +1,9 @@
-
-
 function removeTransition(e) {
 	if (e.propertyName !== 'transform') return;
     e.target.classList.remove('key-pressed');
 }
 
-
 function playSound(e) {
-	console.log(e.keyCode)
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const keyMotorboat = document.querySelector(`.motorboat`);
 	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -16,10 +12,17 @@ function playSound(e) {
 	if(e.altKey === true && e.keyCode === 76) { 
 		document.querySelector(`audio[class="motorboat"]`).play();
 		keyMotorboat.classList.add('key-pressed');
+		keyMotorboat.parentNode.style.opacity = 1; // adds full OPACITY on Motorboat image
+		setTimeout(function(){keyMotorboat.parentNode.style.opacity = 0.6}, 3100); // removes full OPACITY from Motorboat image
 	}
+
 	if(!audio){return};
 	key.classList.add('key-pressed');
+	key.parentNode.style.opacity = 1; // adds full OPACITY on an image
 	audio.play()
+	audio.addEventListener('ended', function() {// removes full OPACITY from an image
+		key.parentNode.style.opacity = 0.6;
+		keyMotorboat.parentNode.style.opacity = 0.6;}) 
 }
 
 const keys = Array.from(document.querySelectorAll('.key'));
