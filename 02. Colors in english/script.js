0b280ac6122182c9243c0ffc414a424c87415f2c
Filip Stangricki <stangricki@gmail.com>
2018-02-23 21:24:37 +0100

    Testing usage of Web speech API in this app

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -1,3 +1,17 @@
+/*//Usage of mp3
+
 document.addEventListener('click', (e) => {
 	e.target.querySelector("audio").play()
 })
+*/
+// Usage of Web speech API - No support for IE and Opera
+
+document.addEventListener('click', (e) => {
+	let color = new SpeechSynthesisUtterance(e.target.className);
+	let chooseVoice = window.speechSynthesis.getVoices();
+	color.voice = chooseVoice[4] // Why first click is default voice???
+	window.speechSynthesis.speak(color);
+})
+
+
+
