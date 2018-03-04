f7374ce5032b6237ed4c711943e10c9723f1ee10
Filip Stangricki <stangricki@gmail.com>
2018-03-04 12:52:11 +0100

    Handling browsers without WebSpeechAPI, using mp3's instead

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -1,17 +1,18 @@
-/*//Usage of mp3
+if ('speechSynthesis' in window){ // Usage of Web speech API - No support for IE and Opera
+	document.addEventListener('click', (e) => {
+		let chooseVoice = window.speechSynthesis.getVoices();
+		let color = new SpeechSynthesisUtterance(e.target.className);
+		color.voice = chooseVoice[4] // Why first click is default voice???
+		window.speechSynthesis.speak(color);
+		console.log('Chrome')
+	})
+} else { //Usage of mp3
+	document.addEventListener('click', (e) => {
+		e.target.querySelector("audio").play()
+		console.log('IE')
+	})
+}
 
-document.addEventListener('click', (e) => {
-	e.target.querySelector("audio").play()
-})
-*/
-// Usage of Web speech API - No support for IE and Opera
-
-document.addEventListener('click', (e) => {
-	let chooseVoice = window.speechSynthesis.getVoices();
-	let color = new SpeechSynthesisUtterance(e.target.className);
-	color.voice = chooseVoice[4] // Why first click is default voice???
-	window.speechSynthesis.speak(color);
-})
 
 
 
