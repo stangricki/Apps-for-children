e7ce290651b5729f8d7ca3210d828133a77b823f
Filip Stangricki <stangricki@gmail.com>
2018-03-04 13:52:26 +0100

    Removed default voice at first click

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -2,6 +2,14 @@ document.addEventListener('click', (e) => {
 	('speechSynthesis' in window) ?	handleClick(e) : handleClickInOtherBrowsers(e)
 })
 
+var speech_voices; // removes asynchronous call to load the voices (default voice on first click)
+if ('speechSynthesis' in window) {
+  speech_voices = window.speechSynthesis.getVoices();
+  window.speechSynthesis.onvoiceschanged = function() {
+    speech_voices = window.speechSynthesis.getVoices();
+  };
+}
+
 function handleClick(e) { // Usage of Web speech API - No support for IE and Opera
 		let chooseVoice = window.speechSynthesis.getVoices();
 		let color = new SpeechSynthesisUtterance(e.target.className);
