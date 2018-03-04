3d881d07bc6384df18135b3bc144c9ead2b953fd
Filip Stangricki <stangricki@gmail.com>
2018-03-04 13:55:51 +0100

    Removed default voice at first click

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -2,20 +2,25 @@ document.addEventListener('click', (e) => {
 	('speechSynthesis' in window) ?	handleClick(e) : handleClickInOtherBrowsers(e)
 })
 
-var speech_voices; // removes asynchronous call to load the voices (default voice on first click)
-if ('speechSynthesis' in window) {
-  speech_voices = window.speechSynthesis.getVoices();
-  window.speechSynthesis.onvoiceschanged = function() {
-    speech_voices = window.speechSynthesis.getVoices();
-  };
-}
-
 function handleClick(e) { // Usage of Web speech API - No support for IE and Opera
-		let chooseVoice = window.speechSynthesis.getVoices();
-		let color = new SpeechSynthesisUtterance(e.target.className);
-		color.voice = chooseVoice[4] // Why first click is default voice???
-		window.speechSynthesis.speak(color);
-	}
+	let chooseVoice = window.speechSynthesis.getVoices();
+	let color = new SpeechSynthesisUtterance(e.target.className);
+	color.voice = chooseVoice[4];
+	window.speechSynthesis.speak(color);
+}
+	
 function handleClickInOtherBrowsers(e){ //Usage of mp3 if there is NO 'speechSynthesis' in window
 	e.target.querySelector("audio").play()
 }
+
+function removeDefaultVoice(){
+	var speech_voices; // removes asynchronous call to load the voices (default voice at first click)
+	if ('speechSynthesis' in window) {
+	  speech_voices = window.speechSynthesis.getVoices();
+	  window.speechSynthesis.onvoiceschanged = function() {
+	    speech_voices = window.speechSynthesis.getVoices();
+	  };
+	}
+}
+
+removeDefaultVoice()
