b3a15f51c099557c41e2067aad8bcb3b5511ebc6
Filip Stangricki <stangricki@gmail.com>
2018-03-04 13:43:35 +0100

    Shorten and clarified code

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -1,18 +1,13 @@
-if ('speechSynthesis' in window){ // Usage of Web speech API - No support for IE and Opera
-	document.addEventListener('click', (e) => {
+document.addEventListener('click', (e) => {
+	('speechSynthesis' in window) ?	handleClick(e) : handleClickInOtherBrowsers(e)
+})
+
+function handleClick(e) { // Usage of Web speech API - No support for IE and Opera
 		let chooseVoice = window.speechSynthesis.getVoices();
 		let color = new SpeechSynthesisUtterance(e.target.className);
 		color.voice = chooseVoice[4] // Why first click is default voice???
 		window.speechSynthesis.speak(color);
-		console.log('Chrome')
-	})
-} else { //Usage of mp3
-	document.addEventListener('click', (e) => {
-		e.target.querySelector("audio").play()
-		console.log('IE')
-	})
+	}
+function handleClickInOtherBrowsers(e){ //Usage of mp3 if there is NO 'speechSynthesis' in window
+	e.target.querySelector("audio").play()
 }
-
-
-
-
