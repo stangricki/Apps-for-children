eb12c3513480ed7c2e0564e34d632d650dedd7ca
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-03-05 20:47:35 +0100

    Refactoring stage 2
    - Enveloped code in a IIFE to prevent poluting the global namespace.
    - Moved handler selection out of the arrow function so the decision is
    made only at startup and not on every click.
    - Gave event handlers more descriptive names, making the comments
    unnecessary.
    - Renamed removeDefaultVoice to init because the function doesn't
    actually remove anything but it initializes the API instead. Moved the
    comment to describe just the specific getVoices call. But could delete
    the comment as well as it doesn't seem to add a lot.

--- 02. Colors in english/script.js
+++ 02. Colors in english/script.js
@@ -1,20 +1,26 @@
-document.addEventListener('click', (e) => {
-	('speechSynthesis' in window) ?	handleClick(e) : handleClickInOtherBrowsers(e);
-});
+(function() {
+	document.addEventListener(
+		'click',
+		'speechSynthesis' in window
+			? playSynth
+			: playAudio
+	);
 
-function handleClick(e) { // Usage of Web speech API - No support for IE and Opera
-	let chooseVoice = window.speechSynthesis.getVoices();
-	let color = new SpeechSynthesisUtterance(e.target.className);
-	color.voice = chooseVoice[4];
-	window.speechSynthesis.speak(color);
-}
-	
-function handleClickInOtherBrowsers(e) { //Usage of mp3 if there is NO 'speechSynthesis' in window
-	e.target.querySelector("audio").play();
-}
+	function playSynth(e) {
+		let chooseVoice = window.speechSynthesis.getVoices();
+		let color = new SpeechSynthesisUtterance(e.target.className);
+		color.voice = chooseVoice[4];
+		window.speechSynthesis.speak(color);
+	}
+		
+	function playAudio(e) {
+		e.target.querySelector("audio").play();
+	}
 
-function removeDefaultVoice() { // Ensures that the voices are loaded by the time we need them
-	window.speechSynthesis.getVoices();
-}
+	function init() {
+		// Ensure that the voices are loaded by the time we need them
+		window.speechSynthesis.getVoices();
+	}
 
-removeDefaultVoice();
+	init();
+}());
